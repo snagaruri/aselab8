18a5085cf46f8941f298e3a50aff3377eaa20c06
sudheerase <nagarurisudheer91@gmail.com>
2016-03-20 04:07:11 -0400

    Update array.js

--- Code/Unit Testing/js/array.js
+++ Code/Unit Testing/js/array.js
@@ -1,19 +1,38 @@
- var app = angular.module('ArrayExample', []);
+ 'user strict';
+describe('Controller:MainCtrl', function () {
+	
+	//load the controller's module
+	beforeEach(module('mytodoApp'));
+	
+	var MainCtrl,
+	scope;
+	
+	//Initialize the controller and a mock scope
+	
+	beforeEach(inject(function($controller,$rootScope) {
+		
+		scope=$rootScope.$new();
+		MainCtrl=$controller('MainCtrl', {
+			$scope: scope
+			
+		});
+	}));
+    
+    it('should have no item to start', function() {
+     expect(scope.todos.length).toBe(0);
+	});
+	
+     it('should add items to the list', function() {
+		scope.todo='Test 1';
+		scope.addTodo();
+	  expect(scope.todos.length).toBe(1);
+	}); 
+	
+	it('should add then remove item from the list', function() {
+		scope.todo='Test 1';
+		scope.addTodo();
+		scope.removeTodo(0);
+	  expect(scope.todos.length).toBe(0);
+	}); 
 
-app.controller('myController', function($scope) {
-    $scope.names = [
-        {firstname:'Ravi kiran',   lastname:'Yadavalli' , country:'India'},
-        {firstname:'John',      lastname:'Doe' ,           country:'USA'},
-        {firstname:'Pradyumna', lastname:'Doddala',        country:'India'},
-        {firstname:'Xing',      lastname:'Wong',           country:'China'},
-        {firstname:'Ragunandan Rao',   lastname:'Malangully' , country:'India'},
-        {firstname:'Guru',      lastname:'Mannava',        country:'India'},
-        {firstname:'Tony',      lastname:'Williams' ,      country:'USA'}
-    ];
-        
-        $scope.fullName= function(first, last) {
-            return last + " " + first;
-        }
-        
-    });
-        
+});	
